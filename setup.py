118b56df822ed5afb68513ab0381d03224212722
Hoàng Thái Thắng <ghant2906@MacBook-cua-ThaiThang.local>
2024-10-30 11:30:28 +0700

    fix sdk

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="residential-proxy-sdk",
-    version="0.1.1",
+    version="0.1.4",
     description="A proxy SDK for managing rotating residential proxies in Vietnam",
     long_description=long_description,
     long_description_content_type="text/markdown",
